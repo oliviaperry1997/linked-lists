f438102e0e77a0eba649530e858f6535bb660495
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-06-06 14:25:43 +0100

    Add append and toString methods and set up main
    This commit adds the append and toString methods and creates main.js,
    which imports the LinkedList class from index.js and populates a list
    with test data.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-class LinkedList {
+export class LinkedList {
     constructor() {
         this.head = null;
         this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.length++;
+    }
+
+    toString() {
+        let current = this.head;
+        let output = "";
+        while (current) {
+            output += current.value + (current.next ? " -> " : "");
+            current = current.next;
+        }
+        return output;
     }
 }
 
